1758f8a66982fdd2d43498e719b52b5473519a84
fengqinghua <fengqinghua@cvte.com>
2023-08-30 17:23:18 +0800

    [modify] 添加本地调试文件

--- test/index.tsx
+++ test/index.tsx
@@ -1,40 +1,28 @@
-import "./style.module.less";
+import style from "./style.module.less";
 import React, { memo } from "react";
 
-//私有常量
-
-//可抽离的逻辑处理函数/组件
-
-let PersonalArchivesTitle = (_props: IProps) => {
+let AverageScoreChart = (_props: IProps) => {
   //变量声明、解构
-
-  //组件状态
-  const state = "aaa";
-
-  //网络IO
-
-  //数据转换
 
   //逻辑处理函数
 
   //组件Effect
 
-  return <div styleName="content active"></div>;
+  return (
+    <div
+      styleName={classNames(style["container"], {
+        [style["changed"]]: changed,
+      })}
+    >
+      {text}
+    </div>
+  );
 };
-
-const style = "outer";
-
-const styles = "styles";
 
 //props类型定义
 interface IProps {}
 
 //prop-type定义，可选
 
-PersonalArchivesTitle = memo(PersonalArchivesTitle);
-export { PersonalArchivesTitle };
-
-<div styleName="content"></div>;
-<div styleName="content active"></div>;
-<div styleName={classNames("content", { active: true })}></div>;
-<div styleName={`content ${true ? "active" : ""}`}></div>;
+AverageScoreChart = observer(AverageScoreChart);
+export { AverageScoreChart };
---
+++ test/test.js
@@ -0,0 +1,6 @@
+const { run } = require('../lib/index');
+
+
+const entry = ["test/*.tsx"]
+
+run(entry)
