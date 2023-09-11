a4c6666a2afd9f4d4d42c04b4adad4b97d3c36f7
fengqinghua <fengqinghua@cvte.com>
2023-09-11 19:54:44 +0800

    [modify] 新增转换computed 属性的插件

--- bin/index.js
+++ bin/index.js
@@ -2,13 +2,20 @@
 
 const argv = require('yargs').argv;
 const path = require('path');
-const runner = require('../lib/index');
+const style2class = require('../lib/stl2cls');
+const removeComputed = require('../lib/removeComputed');
 
 const config = require(path.join(process.cwd(), 'package.json'))['style2class'];
 const command = argv['_'][0];
 
 if (command === 'run') {
     console.info("this task is running...")
-    runner.run(config);
+    style2class.run(config);
     console.info("this task is done!")
+}
+
+if (command === 'remove') {
+    console.info("remove task is running...")
+    removeComputed.run(config)
+    console.info("remove task is done!")
 }
