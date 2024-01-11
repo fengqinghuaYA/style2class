5b0feaaa8c6350dfa8d5115f09112fc917ced47b
fengqinghua <fengqinghua@cvte.com>
2024-01-11 18:26:45 +0800

    feat: 重写

--- test/index.tsx
+++ test/index.tsx
@@ -1,20 +1,45 @@
-import style from "./style.module.less";
-import React, { memo } from "react";
+import style from './style.module.less';
+import React, { memo } from 'react';
+import cls from 'classnames';
 
 let AverageScoreChart = (_props: IProps) => {
-  //变量声明、解构
+    //变量声明、解构
 
-  //逻辑处理函数
+    //逻辑处理函数
 
-  //组件Effect
+    //组件Effect
+    const classNames = 'blue ss';
 
-  return <div className={`${style.container} ${style["blue-aaa"]}`}>sss</div>;
+    const getStyle = () => {};
+
+    return (
+        <div>
+            <div styleName={`dddd-${true && 'active'}`}></div>
+            <div styleName={true && 'active'}></div>
+            <div styleName={true ? cls('content', `${true && cls('content')}`) : ''}></div>
+            <div styleName="content"> </div>
+            <div styleName="content blue"> </div>
+            <div styleName={classNames}></div>
+            <div
+                styleName={cls(
+                    'content content-1',
+                    true && 'content content-2',
+                    {
+                        disabled: true,
+                        ['blue-1']: true,
+                    },
+                    ['white', false && 'red', true ? 'white' : 'red']
+                )}
+            ></div>
+
+            <div styleName={cls('aa')}></div>
+
+            <div className="anana"></div>
+        </div>
+    );
 };
 
 //props类型定义
 interface IProps {}
 
 //prop-type定义，可选
-
-AverageScoreChart = observer(AverageScoreChart);
-export { AverageScoreChart };
--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 const { run } = require('../lib/stl2cls');
 
+const entry = ['test/*.tsx'];
 
-const entry = ["test/*.tsx"]
-
-run({ entry, computed: false })
+run({ entry, computed: false });
